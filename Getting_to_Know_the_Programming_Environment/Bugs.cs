d9c92bd5c47d4725f650912bc4aab0329e2314e3
Paulina Kucharczyk <paulinapat96@gmail.com>
2026-01-28 22:58:28 +0100

    Create Bugs.cs

---
+++ Getting_to_Know_the_Programming_Environment/Bugs.cs
@@ -0,0 +1,30 @@
+// PROGRAM TO FIX – find and correct all errors!
+class Bugs
+{
+    public static void Start()
+    {
+        // PROGRAM TO FIX – find and correct all errors!
+        Console.writeline("Welcome to the cinema!") 
+
+        Console.WriteLine("What is your name?);
+        string name = Console.ReadLine()      
+
+        int age = "12"; 
+
+        bool hasTicket;
+        Console.WriteLine("Do you have a ticket? (yes/no)");
+        string answer = Console.ReadLine();
+        hasTicket = answer == "yes"           
+
+        if (age >= 13 && hasTicket = true    
+        {
+            Console.WriteLine($"Welcome, {name}!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, this movie is for ages 13+");
+        ) 
+
+        Console.WriteLine("Goodbye, " + name);; 
+    }
+}
